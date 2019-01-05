a35c5627f6d1b786a150bc18435cf1cdf53a124e
dineshkumar227 <dkr2@illinois.edu>
2019-01-05 11:10:50 +0530

    Moved classes to separate files

--- OpenMW_AutoMod.rb
+++ OpenMW_AutoMod.rb
@@ -4,112 +4,12 @@ require 'pry'
 require 'yaml'
 require 'open-uri.rb'
 require 'mechanize'
+require 'ModFlig'
+require 'NexusMods'
+
 @run = false
-@download_directory = "/home/dinesh/RubymineProjects/OpenMW_AutoMod"
-
+@download_directory = ""
 #Watir.default_timeout = 60000
-
-class NexusMods
-
-  def login
-    browser = open_browser("nexusmods.com")
-    puts "Press Enter when logged in"
-    gets
-    return browser
-  end
-
-  def find_download_button (browser)
-    browser.li.wait_until_present
-    urls = browser.lis.to_a
-    urls.each do |url|
-      if url.id.include?'action-manual'
-        download_button = url.children[0].children[0]
-        return download_button
-      end
-    end
-  end
-
-  def download(links, link_numbers)
-    browser = NexusMods.login
-    combined_links = links.zip(link_numbers)
-    combined_links.each do |link, link_number|
-      browser.goto(link)
-      download_button = NexusMods.find_download_button(browser)
-      begin
-        download_button.click
-      rescue
-        puts "Make browser fullscreen"
-        gets
-        retry
-      end
-      sleep(5)
-      puts link_number
-      append_file(link_number)
-    end
-  end
-
-end
-
-class ModFlig
-
-  def download(links, link_numbers)
-    combined_links = links.zip(link_numbers)
-    combined_links.each do |link, link_number|
-      file_link = ModFlig.convert_link(link)
-      download_file = ModFlig.mechanize_download(file_link, link_number)
-      Dir.chdir(@download_directory)
-      download_file.save
-    end
-  end
-
-  def convert_link(link)
-    link_parts = link.split(/(\/+)|(-)/)
-    file_link = "http://" + link_parts[2] + "/file.php?id=" + link_parts[8]
-    return file_link
-  end
-
-  def mechanize_download(file_link, link_number)
-    agent = Mechanize.new
-    download_file = agent.get(file_link)
-    download_file.filename = link_number.to_s + download_file.filename
-    return download_file
-  end
-
-end
-
-class ModdingOpenMW
-
-  def find_links(link)
-    url = link.children[0].children[0].href
-    if url.start_with? 'https://modding-openmw.com/mods/steps/?step='
-      return link.children[1].children[0].href
-    end
-  end
-
-  def convert_elements(mod_type)
-    browser = open_browser(mod_type)
-    browser.tr.wait_until_present
-    all_links = browser_link.trs
-    links_arr = all_links.to_a
-    links_arr.shift
-    return links_arr
-  end
-
-  def scrape (mod_type)
-    download_links = []
-    links_arr = convert_elements(mod_type)
-    puts "Scraping links from:" + mod_type + "..."
-    links_arr.each do |link|
-      download_link = find_links(link)
-      download_links.push download_link
-      puts download_link
-    end
-    puts "#{download_links.length} links collected"
-    browser_link.close
-    return download_links
-  end
-
-end
 
 def open_browser(link)
   unless @run
@@ -127,9 +27,13 @@ def open_browser(link)
   return browser
 end
 
-def append_file(number)
+def get_latest_file
   Dir.chdir(@download_directory)
   file_directory = Dir.glob(@download_directory + "/*.*").max_by {|f| File.mtime(f)}
+end
+
+def append_file(number)
+  file_directory = get_latest_file 
   directory_parts = file_directory.split('/')
   filename = number.to_s + directory_parts.last
   binding.pry
